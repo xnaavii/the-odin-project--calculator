85cfba6c4698d82e072c010badc5cd57720d3455
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-04-22 16:24:06 +0100

    Create basic arithmetic operator functions

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const calculator = document.querySelector('#calculator');
 
+// Basic arithmetic operations
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
