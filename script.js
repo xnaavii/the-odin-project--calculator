17e0fd2e481490362b84bbe0286e0b0f6f15a16b
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-04-22 16:26:25 +0100

    Add operate function

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const calculator = document.querySelector('#calculator');
 
+let a = 0;
+let b = 0;
+let operator = 0;
+
+const operate = (operator, a, b) => operator(a, b);
+
 // Basic arithmetic operations
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
