901e3f6879ebeb77183d5557e3d9f5dd841c9b4a
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-04-22 16:36:07 +0100

    Add data attribute and extract data to update ui

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const calculator = document.querySelector('#calculator');
+const firstNumber = document.querySelector('#firstNumber');
+const digits = document.querySelectorAll('.digit');
 
 let a = 0;
 let b = 0;
 let operator = 0;
+
+const updateFirstNumber = (value) => {
+  a = value;
+  firstNumber.textContent = a;
+};
+
+digits.forEach((d) =>
+  d.addEventListener('click', (e) => {
+    const value = d.dataset.value;
+    updateFirstNumber(value);
+  }),
+);
 
 const operate = (operator, a, b) => operator(a, b);
 
